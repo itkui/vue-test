6d8d6ece1c9dd92bf9c04c588dac8ba5656c1cf9
清风一缕 <13452903663@163.com>
2020-05-06 13:14:10 +0800

    初始化仓库

---
+++ src/components/Jsx.vue
@@ -0,0 +1,8 @@
+<script>
+export default {
+  render(createElement) {
+      console.log(createElement('div', this.$slots.default))
+    return createElement('div', this.$slots.default);
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <jsx>这里是一段slot插槽</jsx>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Jsx from '@/components/Jsx'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Jsx
   }
 }
 </script>
