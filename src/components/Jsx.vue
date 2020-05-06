7e9375bcc27d26318b27c32388d98847e0b9be8d
清风一缕 <13452903663@163.com>
2020-05-06 13:21:26 +0800

    提交

--- src/components/Jsx.vue
+++ src/components/Jsx.vue
@@ -1,8 +1,30 @@
 <script>
 export default {
   render(createElement) {
-      console.log(createElement('div', this.$slots.default))
-    return createElement('div', this.$slots.default);
+    return createElement(
+      'div',
+      {
+        class: {
+          foo: true,
+          bar: false,
+        },
+        style: {
+          color: 'red',
+        },
+        attrs: {
+          id: 'foo',
+        },
+        props: {
+          myProp: {
+            type: Array,
+            default() {
+              return [1, 2, 3];
+            },
+          },
+        },
+      },
+      this.$slots.default,
+    );
   },
 };
 </script>
